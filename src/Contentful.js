3ce9cfbfca5b14328625182e986e699c8545d757
Cristiano <cristianoeeria@gmail.com>
2019-11-29 12:17:32 -0300

    acessos

--- src/Contentful.js
+++ src/Contentful.js
@@ -2,5 +2,5 @@ import {createClient} from 'contentful'
 
 export default createClient({
     space:process.env.REACT_APP_LOGIN,
-    accessToken:process.env.RECAT_APP_KEYCODE
+    accessToken:process.env.REACT_APP_KEYCODE
 })

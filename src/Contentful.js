b9de87bd8b351add2f0381b69e4a12c43d330a0f
Cristiano <cristianoeeria@gmail.com>
2019-11-29 16:20:43 -0300

    imagens

--- src/Contentful.js
+++ src/Contentful.js
@@ -1,8 +1,8 @@
 import {createClient} from 'contentful'
 
 export default createClient({
-    /*space:process.env.REACT_APP_LOGIN,
-    accessToken:process.env.REACT_APP_KEYCODE*/
-    space:"hx3tqtdghre7",
-    accessToken:"ikgpeFNSb3kCXF6qtdRCFIqurDDF27E0RzwZH63oxR8"
+    space:process.env.REACT_APP_LOGIN,
+    accessToken:process.env.REACT_APP_KEYCODE
+    /*space:"hx3tqtdghre7",
+    accessToken:"ikgpeFNSb3kCXF6qtdRCFIqurDDF27E0RzwZH63oxR8"*/
 })

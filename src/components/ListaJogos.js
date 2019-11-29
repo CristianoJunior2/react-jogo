5677960e1003c31ee813de3f87426a4732561909
CristianoJunior2 <45183953+CristianoJunior2@users.noreply.github.com>
2019-11-29 02:06:29 -0300

    Update ListaJogos.js

--- src/components/ListaJogos.js
+++ src/components/ListaJogos.js
@@ -6,7 +6,7 @@ export default function ListaJogos({jogos}) {
 
     if (jogos.length === 0) {
         return (
-            <>Nada</>
+            <h3>Nenhum jogo encontrado<h3/>
         )
     }
 
